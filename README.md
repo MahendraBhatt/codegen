af90a18574076ba56fb794291e019ed90b166f15
Mahendra Bhatt <bhatmahendramulraj@gmail.com>
2015-12-01 21:47:56 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # codegen
 code generator using node
+
+Use npm link to add this project to local node_modules
+
+Use npm link codegen to add this project as dependency to target project
---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+///////////////////////////////////////
+// Required
+///////////////////////////////////////
+
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+    del = require('del')
+
